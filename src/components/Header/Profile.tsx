bec6e99364194f437c418d9e82344c9775dd5919
André Torquato <ats.torquato1@gmail.com>
2021-05-02 01:05:19 -0300

    :construction:styles: responsive SidebarNav and responsive Headear

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,19 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>André Torquato</Text>
-            <Text color="gray.300" fontSize="small">
-              ats.torquato1@gmail.com
-            </Text>
-          </Box>
-          <Avatar
-            size="md"
-            name="André Torquato"
-            src="https://github.com/andretorquato.png"
-          ></Avatar>
-        </Flex>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>André Torquato</Text>
+          <Text color="gray.300" fontSize="small">
+            ats.torquato1@gmail.com
+          </Text>
+        </Box>
+      )}
+      <Avatar
+        size="md"
+        name="André Torquato"
+        src="https://github.com/andretorquato.png"
+      ></Avatar>
+    </Flex>
   );
 }
