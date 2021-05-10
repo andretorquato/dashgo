3218b436985901b5fd2cbeca38bfdcd37ea39c3e
André Torquato <ats.torquato1@gmail.com>
2021-05-09 23:43:16 -0300

    :sparkles:feat: Listing users and pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,26 +13,24 @@ import {
   Td,
   Text,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
-import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import Link from "next/link";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
+  const { data, isLoading, isFetching, error } = useUsers();
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() =>{
-    fetch('http://localhost:3000/api/users')
-    .then(response => response.json())
-    .then(data => console.table(data));
-  }, []);
-
+  
   return (
     <Box>
       <Header />
@@ -42,7 +40,8 @@ export default function UserList() {
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
-              Usuários
+              Usuários 
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
             </Heading>
             <Link href="/users/create" passHref>
               <Button
@@ -57,31 +56,43 @@ export default function UserList() {
             </Link>
           </Flex>
 
+         { isLoading ? (
+           <Flex justify="center">
+             <Spinner />
+           </Flex>
+        ) : error ?  (
+          <Flex justify="center">
+            <Text>Falha ao carregar os dados</Text>
+          </Flex>
+        ) : (
+          <>
           <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
+          <Thead>
+            <Tr>
+              <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                <Checkbox colorScheme="pink" />
+              </Th>
+              <Th>Usuário</Th>
+              {isWideVersion && <Th>Data de cadastro</Th>}
+              <Th width="8"></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            { data.map( user => {
+              return (
+                <Tr key={user.id}>
                 <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight="bold">André Torquato</Text>
+                    <Text fontWeight="bold">{user.name}</Text>
                     <Text fontSize="sm" color="gray.300">
-                      ats.torquato@gmail.com
+                      {user.email}
                     </Text>
                   </Box>
                 </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                {isWideVersion && <Td>{user.createdAt}</Td>}
                 <Td>
                   {isWideVersion && (
                     <Button
@@ -96,63 +107,17 @@ export default function UserList() {
                   )}
                 </Td>
               </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">André Torquato</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      ats.torquato@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">André Torquato</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      ats.torquato@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  {isWideVersion && (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  )}
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+              );
+            })}
+          </Tbody>
+        </Table>
+        <Pagination 
+        totalCountOfRegister={200}
+        currentPage={19}
+        onPageChange={() => {}}
+        />
+        </>
+        )}
         </Box>
       </Flex>
     </Box>
--- src/services/mirage.ts
+++ src/services/mirage.ts
@@ -1,44 +1,57 @@
-import { createServer, Factory, Model } from 'miragejs';
-import faker from 'faker';
+import { createServer, Factory, Model, Response } from "miragejs";
+import faker from "faker";
 
 type User = {
   name: string;
   email: string;
   created_at: string;
-}
-export function makeServe(){
+};
+export function makeServe() {
   const server = createServer({
     models: {
-      user: Model.extend<Partial<User>>({})
+      user: Model.extend<Partial<User>>({}),
     },
-    factories:{
+    factories: {
       user: Factory.extend({
-        name(i:number){
-          return `User ${ i + 1}`
+        name(i: number) {
+          return `User ${i + 1}`;
         },
-        email(){
+        email() {
           return faker.internet.email().toLowerCase();
         },
-        createdAt(){
+        createdAt() {
           return faker.date.recent(10);
-        }
-          
-      })
+        },
+      }),
     },
-    seeds(server){
-        server.createList('user', 200);
+    seeds(server) {
+      server.createList("user", 200);
     },
-    routes(){
-      this.namespace = 'api';
+    routes() {
+      this.namespace = "api";
       this.timing = 750;
-      
-      this.get('/users');
-      this.post('/users');
 
-      this.namespace = '';
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
+      this.post("/users");
+
+      this.namespace = "";
       this.passthrough();
-    }
+    },
   });
 
   return server;
-};
+}
